ec9c70682787faee399ca55c7f354a0de9ff3538
Aldia <128320079+aldianugraha@users.noreply.github.com>
2023-06-26 06:56:54 +0700

    Add files via upload

---
+++ a.js
@@ -0,0 +1,20 @@
+function update(e) {
+  var x = e.clientX || e.touches[0].clientX;
+  var y = e.clientY || e.touches[0].clientY;
+
+  document.documentElement.style.setProperty("--cursorX", x + "px");
+  document.documentElement.style.setProperty("--cursorY", y + "px");
+}
+
+document.addEventListener("mousemove", update);
+document.addEventListener("touchmove", update);
+
+function initializeMap() {
+  console.log("initialize");
+  var myOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+}
